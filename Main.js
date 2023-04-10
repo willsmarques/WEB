c8998d50f5ca4a95b4459c89813f649b5c0723dd
William Marques <willsmarques@gmail.com>
2023-04-10 11:01:19 -0300

    Projetos WEB

---
+++ Main.js
@@ -0,0 +1,15 @@
+
+
+let firstName = prompt('Digite o seu Primeiro nome:')
+let lastName = prompt('Digite o seu sobre nome:')
+let age = parseInt(prompt('Digite a sua idade: '))
+
+age = age +2
+
+
+console.log(firstName)
+console.log(lastName)
+console.log(age)
+
+
+
