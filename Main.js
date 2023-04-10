d015253c288425c4924600ba916de89e4051292f
William Marques <willsmarques@gmail.com>
2023-04-10 11:30:53 -0300

    modificando css

--- Main.js
+++ Main.js
@@ -1,15 +1,16 @@
 
 
-let firstName = prompt('Digite o seu Primeiro nome:')
-let lastName = prompt('Digite o seu sobre nome:')
-let age = parseInt(prompt('Digite a sua idade: '))
+///let firstName = prompt('Digite o seu Primeiro nome:')
+///let lastName = prompt('Digite o seu sobre nome:')
+//let age = parseInt(prompt('Digite a sua idade: '))
+//const age2 = 2
 
-age = age +2
+///age = age +age2
 
 
-console.log(firstName)
-console.log(lastName)
-console.log(age)
+//console.log(firstName)
+//console.log(lastName)
+//console.log(age)
 
 
 
