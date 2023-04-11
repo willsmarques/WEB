411aa98793b7e5232e4307bd9b3a89d16568f07f
William Marques <willsmarques@gmail.com>
2023-04-11 15:29:57 -0300

    Adivinhar um número

--- Main.js
+++ Main.js
@@ -1,6 +1,29 @@
-function changBackgroundColor(){
-    let colors = ['red','green','yellow','orange','pink']
-    let randomColor = colors[Math.floor(Math.random() * colors.length)]
-    document.body.style.backgroundColor = randomColor
+let randomNumber = Math.floor(Math.random() *100)
+let tries = 0
+//console.log(randomNumber)
+
+
+function checkGuess(){
+
+    let guess = document.getElementById("guess").value
+    let message = document.getElementById('message')
+    tries++
+
+    if  (guess ==  randomNumber){
+
+       message.innerHTML = 'Voce acertou o numero, com ' + tries + 'Tentativas'  
+
+    }
+    else if (guess < randomNumber){
+
+        message.innerHTML = 'Valor acima do escolhido'  
+
+    }
+    else {
+
+        message.innerHTML = 'Valor abaixo  do escolhido'  
+
+    }
+
 }
 
